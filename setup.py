57821ef263a079784397e17b5889312d9d1a8d20
grupocolav <67655643+grupocolav@users.noreply.github.com>
2021-04-30 19:37:53 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -54,7 +54,7 @@ def main():
         name="Chibchas",
 
         # Version number (initial):
-        version="0.1.3",
+        version="0.1.3-beta",
 
         # Application author details:
         author="Colav",
