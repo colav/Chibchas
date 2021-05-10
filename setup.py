c66059d54afb6c6c8d0ab444da4ced077ff8f618
Diego Restrepo <restrepo@udea.edu.co>
2021-05-09 21:44:50 -0500

    Add COL_Group option to search for Group directly

--- setup.py
+++ setup.py
@@ -54,7 +54,7 @@ def main():
         name="Chibchas",
 
         # Version number (initial):
-        version="0.2.2-beta",
+        version="0.2.3-beta",
 
         # Application author details:
         author="Colav",
