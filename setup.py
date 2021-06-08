d17c64faa479de7222b8734d483cf49b1876e8ca
Grupo Colav <grupocolav@udea.edu.co>
2021-06-08 18:39:16 +0200

    changed version

--- setup.py
+++ setup.py
@@ -54,7 +54,7 @@ def main():
         name="Chibchas",
 
         # Version number (initial):
-        version="0.2.5-beta",
+        version="0.2.6-beta",
 
         # Application author details:
         author="Colav",
