d1a99ce64f54ab96c5273d0287e2c50fe4cf7ae9
Grupo Colav <grupocolav@udea.edu.co>
2021-06-12 20:09:01 +0200

    fixed bug in find packages

--- setup.py
+++ setup.py
@@ -61,8 +61,7 @@ def main():
         author_email="colav@udea.edu.co",
 
         # Packages
-        package_dir={"": "chibchas"},
-        packages=find_packages(where="chibchas",exclude=['tests']),
+        packages=find_packages(exclude=['tests']),
         python_requires=">=3.6",
 
         # Include additional files into the package
