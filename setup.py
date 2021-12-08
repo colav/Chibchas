75d92764eb6676c28f937e0ccb8cd4529fba4a7e
RWC <39197176+readingwritingcode@users.noreply.github.com>
2021-12-07 20:58:14 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -112,7 +112,7 @@ def main():
         # that you indicate whether you support Python 2, Python 3 or both.
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
+        #'Programming Language :: Python :: 3.7', avoid dependency conflicts
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
         ],
